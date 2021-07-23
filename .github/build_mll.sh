a26681c5066164a2761abd586f33915a94d72c8d
berlin4apk <83521068+berlin4apk@users.noreply.github.com>
2021-07-24 01:24:56 +0200

    Update build_mll.sh

--- .github/build_mll.sh
+++ .github/build_mll.sh
@@ -6,7 +6,9 @@ set -e
 
 cd ../src
 
+echo "::group::Group name apt-get build depends"
 sudo apt-get -qq -y install wget make gawk gcc bc xz-utils bison flex xorriso libelf-dev libssl-dev
+echo "::endgroup::"
 
 PREFIXES="00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16"
 
